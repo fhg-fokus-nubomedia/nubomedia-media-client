f68a51e04f6b56a6b84506cc3734cda96f06234c
Alice Cheambe <alice.cheambe@fokus.fraunhofer.de>
2016-02-16 12:48:10 +0100

    code cleaning

--- src/main/java/de/fhg/fokus/nubomedia/paas/ApplicationRecord.java
+++ src/main/java/de/fhg/fokus/nubomedia/paas/ApplicationRecord.java
@@ -1,8 +1,16 @@
-/**
+/*******************************************************************************
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  *
  * @author Alice Cheambe <alice.cheambe[at]fokus.fraunhofer.de>
- * 
- */
+ *******************************************************************************/
+
 package de.fhg.fokus.nubomedia.paas;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -17,11 +25,6 @@ public class ApplicationRecord {
 	private String mediaServerId;
 	private String extAppId;
 	
-//	public ApplicationRecord(String vnfrId, String extId, int points){
-//		this.vnfrId = vnfrId;
-//		this.externalAppId = extId;
-//		loadPoints = points;
-//	}
 
 	public String getInternalAppId() {
 		return id;
